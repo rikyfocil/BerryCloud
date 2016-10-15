e06a6cef9c276d18cd98ad495e7eee4a782ce964
Ricardo Lopez Focil <rikyfocil@Ricardos-MacBook-Pro.local>
2016-10-15 16:17:20 -0500

    Moved the errors to a common format with blade and created a 404 view

--- resources/views/errors/501.blade.php
+++ resources/views/errors/501.blade.php
@@ -1,47 +1,12 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Comming soon</title>
+@extends('layouts.error')
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+@section('title', 'Comming soon')
 
-        <style>
-            html, body {
-                height: 100%;
-            }
+@section('message_title', 'We are almost there')
 
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato', sans-serif;
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 72px;
-                margin-bottom: 40px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">We are almost there</div>
-                <p>BerryCloud is still in devolpment and this feature is not yet implemented<br>Stay tuned</p>
-            </div>
-        </div>
-    </body>
-</html>
+@section('message_content')
+    <p>
+        BerryCloud is still in devolpment and this feature is not yet 
+        implemented <br> Stay tuned
+    </p>
+@endsection
