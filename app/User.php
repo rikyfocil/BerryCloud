98877b03373ef1d9cfe05e664ac39f5685cd5cae
rikyfocil <rikyfocil@users.noreply.github.com>
2016-11-29 15:47:22 -0600

    Error fixing (#28)

--- app/User.php
+++ app/User.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Carbon\Carbon;
 
 class User extends Authenticatable
 {
@@ -42,4 +43,57 @@ class User extends Authenticatable
     public function ownedGroups(){
         return $this->hasMany('App\Group', 'idOwner');
     }
+
+    public function sharedWithMe(){
+
+
+        $fileArray = $this->sharedWithUser($this);
+        
+        foreach ( $this->groupMemberships()->get() as $membership ) {
+            $user = $membership->group()->first()->representativeUser()->first();
+            $fileArray = array_merge ( $fileArray, $this->sharedWithUser($user) );
+        }
+
+        $filtered = [];
+        $count = count($fileArray);
+
+        for($i = 0; $i < $count; $i++){
+
+            $found = false;
+
+            for($j = $i + 1; $j < $count; $j++ ){
+                if($fileArray[$i]->id == $fileArray[$j]->id){
+                    $found = true;
+                    break;
+                }
+            }
+
+            if(!$found){
+                array_push($filtered, $fileArray[$i]);
+            }
+
+        }
+
+        return $filtered;
+    }
+
+    private function sharedWithUser($user){
+
+        $sharedWithMe = Share::where('idUser', $user->id)->where( function($query){ 
+            $query->where('dueDate', null)->orWhere('dueDate', '>', Carbon::now() );
+        } )->get();
+        
+        $fileArray = [];
+
+        foreach ($sharedWithMe as $currentShare) {
+            $file = $currentShare->file()->first();
+
+            if($file)
+                array_push($fileArray, $file);
+        }
+
+        return $fileArray;
+    }
+
+
 }
